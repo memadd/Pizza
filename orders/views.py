c1579c6775bd3ea6382daf28ea4fe2ff58150e4b
memadd <memad632@gmail.com>
2020-02-27 19:01:45 -0800

    completed the menu

--- orders/views.py
+++ orders/views.py
@@ -5,12 +5,18 @@ from django.shortcuts import render
 from django.views import generic
 from django.views.generic import View
 
-from .models import Order
+from .models import Order, Toppings
 
 
 def index(request):
     context = {
-        "orders": Order.objects.all()
+        "pizza": Order.objects.all()[:2],
+        "subs": Order.objects.all()[2:16],
+        "pasta": Order.objects.all()[16:19],
+        "salads": Order.objects.all()[19:23],
+        "plates": Order.objects.all()[23:30],
+        "toppings":Toppings.objects.all()
+
     }
     return render(request, "orders/index.html", context)
 
